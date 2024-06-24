4250c58b328c60f2af400e8f0ebde128273b8da3
Young <liamTT121@gmail.com>
2024-06-24 21:26:59 +0800

    Fix bug in q_remove_head() and q_remove_tail()
    Need to return element deleted even if sp is NULL.

--- queue.c
+++ queue.c
@@ -103,12 +103,15 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
     element_t *e;
 
-    if (!head || list_empty(head) || !sp)
+    if (!head || list_empty(head))
         return NULL;
 
     e = list_first_entry(head, element_t, list);
-    strncpy(sp, e->value, bufsize);
-    sp[bufsize - 1] = '\0';
+    if (sp) {
+        strncpy(sp, e->value, bufsize);
+        sp[bufsize - 1] = '\0';
+    }
+
     list_del_init(&e->list);
     return e;
 }
@@ -118,12 +121,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
     element_t *e;
 
-    if (!head || list_empty(head) || !sp)
+    if (!head || list_empty(head))
         return NULL;
 
     e = list_last_entry(head, element_t, list);
-    strncpy(sp, e->value, bufsize);
-    sp[bufsize - 1] = '\0';
+    if (sp) {
+        strncpy(sp, e->value, bufsize);
+        sp[bufsize - 1] = '\0';
+    }
+
     list_del_init(&e->list);
     return e;
 }
