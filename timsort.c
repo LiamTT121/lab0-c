da6687806297ce106602ed61bfce40dd767f533f
Young <liamTT121@gmail.com>
2024-07-14 18:41:45 +0800

    Implement constructor of runs_queue

---
+++ timsort.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+#include "list.h"
+#include "timsort.h"
+
+#define MIN_RUN_SIZE 10
+
+struct element {
+    struct list_head list;
+    int value;
+};
+
+struct run {
+    struct list_head *head;
+    size_t size;
+};
+
+struct runs_queue {
+    struct list_head *head;
+    size_t count;
+};
+
+static struct runs_queue *new_runs_queue()
+{
+    struct runs_queue *rq = calloc(1, sizeof(*rq));
+
+    if (!rq)
+        goto error;
+
+    rq->head = malloc(sizeof(struct list_head));
+    if (!rq->head) {
+        free(rq);
+        goto error;
+    }
+    INIT_LIST_HEAD(rq->head);
+
+    return rq;
+
+error:
+    printf("Fail to construct new queue of runs");
+    exit(1);
+}
+
+static int cmp_func(void *priv,
+                    const struct list_head *a,
+                    const struct list_head *b)
+{
+    // todo;
+    return 0;
+}
+
+static void merge()
+{
+    // todo;
+}
+
+static void timsort(void *priv, struct list_head *head, list_cmp_func_t cmp)
+{
+    struct runs_queue *all_run = new_runs_queue();
+
+    /* just used to pass the static checking (unused variable)
+     * need to clear this after implementation */
+    all_run->count = 0;
+    struct run delete_me;
+    struct element delete_me_too;
+    delete_me_too.value = 0;
+    delete_me.head = NULL;
+    delete_me.size = delete_me_too.value;
+    printf("timsort is wrong if you see this message. %zu\n", delete_me.size);
+    /* clear until here */
+
+    // todo;
+}
---
+++ timsort.h
@@ -0,0 +1,14 @@
+#ifndef TIMSORT_H
+#define TIMSORT_H
+
+#include "list.h"
+
+typedef int
+    __attribute__((nonnull(2, 3))) (*list_cmp_func_t)(void *,
+                                                      const struct list_head *,
+                                                      const struct list_head *);
+
+__attribute__((nonnull(2, 3))) timsort(void *priv,
+                                       struct list_head *head,
+                                       list_cmp_func_t cmp);
+#endif
